6091b2e94fea5fc8d4d4d79b1568bc842fcd6486
user <user@example.com>
2026-03-13 21:36:03 +0000

    feat: update instruction font size and add title

--- app/page.tsx
+++ app/page.tsx
@@ -13,23 +13,24 @@ export default function Home() {
       </div>
 
       <div className="mt-16 text-left max-w-4xl w-full">
+        <h2 className="text-3xl font-bold mb-6 text-center">How to Use</h2>
         <div className="mb-8">
-          <h3 className="text-2xl font-semibold mb-2">1. Select Videos:</h3>
-          <p className="text-gray-300">
+          <h3 className="text-xl font-semibold mb-2">1. Select Videos:</h3>
+          <p className="text-gray-400">
             Use the calendar or the folder list to select the desired ski video date. Click once on 2 videos and they will download and show up on the screen. When you wish to change the selected videos, navigate back to the video list and click on the current videos once more to unselect them, and click on and select a new set of videos to compare.
           </p>
         </div>
 
         <div className="mb-8">
-          <h3 className="text-2xl font-semibold mb-2">2. Align Videos for Analysis:</h3>
-          <p className="text-gray-300">
+          <h3 className="text-xl font-semibold mb-2">2. Align Videos for Analysis:</h3>
+          <p className="text-gray-400">
             Use the sliders to align the videos at a desired point in the course. Click the “Mark Sync Point” button for each video to set the play points. You also have the ability to mark a second sync point for more detailed analysis; move the sliders to line the videos up at a new point, and mark the sync points once more to track comparisons in a shorter, bounded time period. Press the “Clear Sync Point” button to reset the sync points and select new ones.
           </p>
         </div>
 
         <div className="mb-8">
-          <h3 className="text-2xl font-semibold mb-2">3. Viewing Functions:</h3>
-          <p className="text-gray-300">
+          <h3 className="text-xl font-semibold mb-2">3. Viewing Functions:</h3>
+          <p className="text-gray-400">
             Once your videos are prepared, press the play button to start the videos from your sync point. Pause the video at a time of your choice and compare the videos. While a video is paused, you can use the arrow buttons to fast forward frames, 2 at a time, or 4 at a time, backwards or forwards. Resume the video to continue watching.
           </p>
         </div>
