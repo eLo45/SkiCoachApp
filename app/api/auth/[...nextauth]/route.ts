04bb24be6b8158a8b24b72e688c8f6c2d86380dd
user <user@example.com>
2026-03-13 21:11:49 +0000

    Merge branch 'main' of https://github.com/eLo45/SkiCoachApp

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,22 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth"
+import NextAuth, { Account, Profile } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }: { account: Account | null, profile?: Profile & { email_verified?: boolean, email?: string } }) {
+      if (account?.provider === "google") {
+        return !!(profile?.email_verified && profile?.email?.endsWith("@cardigan.org"))
+      }
+      return true
+    },
+  }
+}
 
 const handler = NextAuth(authOptions)
 
